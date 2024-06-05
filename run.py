dd7d95f44a2b47f3d663b35c7b0155362f4e32d5
NoamBuko <noambuko10@gmail.com>
2024-06-05 14:06:36 +0300

    a

---
+++ run.py
@@ -0,0 +1,38 @@
+import pygame
+from settings import *
+from building import Building
+
+building = Building()
+
+pygame.init()
+
+# draw screen 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+clock = pygame.time.Clock()
+background = pygame.image.load(BACKGROUND_PIC)
+background = pygame.transform.scale(background, ((SCREEN_WIDTH, SCREEN_HEIGHT)))
+
+# run while loop
+run = True
+while run:
+    clock.tick(60)
+    # screen.fill('white')
+    screen.blit(background, (0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = event.pos
+    
+            building.check_for_new_calls(position)
+   
+
+    building.update_all()
+    building.draw_all(screen)
+    
+    pygame.display.update()
+
+pygame.quit()
